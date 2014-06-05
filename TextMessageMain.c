d7fdb239c91c4b78dfbfc14eaa3573d47334ffa8
pokemane <j.d.cravener@gmail.com>
2014-06-04 21:36:07 -0400

    I pushed buttons too fast I dun screwed up, man.  changed stuff back

--- TextMessageMain.c
+++ TextMessageMain.c
@@ -35,9 +35,9 @@ uint16_t timer10Hz = 0x0002;
 uint16_t timer1Hz = 0x0001;
 
 uint16_t hourButton = 0x0010;
-uint16_t minButton	= 0x0020;
+uint16_t minButton = 0x0020;
 
-uint16_t txtRx 		= 0x0001;
+uint16_t txtRx = 0x0001;
 
 uint16_t dispClock = 0x0001;
 uint16_t dispUser = 0x0002;
@@ -47,7 +47,7 @@ uint16_t dispUser = 0x0002;
 */
 
 OS_MUT mut_osTimestamp;
-Timestamp osTimestamp = {0,0,0};
+Timestamp osTimestamp = { 0, 0, 0 };
 
 OS_MUT mut_msgList;
 OS_MUT mut_freeList;
@@ -58,7 +58,7 @@ List *lstStr;
 uint8_t *timeToString(void);
 
 // delcare mailbox for serial buffer
-os_mbx_declare(mbx_MsgBuffer,4);
+os_mbx_declare(mbx_MsgBuffer, 4);
 
 __task void InitTask(void);
 __task void TimerTask(void);
@@ -75,17 +75,17 @@ OS_TID idDispTask;
 void SerialInit(void);
 
 
-int main (void){
+int main(void){
 	// initialize hardware
 	SerialInit();
 	JOY_Init();
 	LED_Init();
 	SRAM_Init();
 	GLCD_Init();
-	
+
 	// InitTask
 	os_sys_init_prio(InitTask, 250);
-	
+
 }
 
 
@@ -100,17 +100,17 @@ int main (void){
 
 __task void InitTask(void){
 	// initialize semaphores
-	os_sem_init(&sem_Timer10Hz,0);
-	os_sem_init(&sem_Timer1Hz,0);
+	os_sem_init(&sem_Timer10Hz, 0);
+	os_sem_init(&sem_Timer1Hz, 0);
 	os_sem_init(&sem_TextRx, 0);
-	os_sem_init(&sem_dispUpdate,0);
-	
+	os_sem_init(&sem_dispUpdate, 0);
+
 	// initialize mutexes
 	os_mut_init(&mut_osTimestamp);
 	// the message input queue
 	lstRXQ->count = 4;
 	lstRXQ->first = NULL;
-	lstRXQ->last  = NULL;
+	lstRXQ->last = NULL;
 	// lstRXQ->startAddr = MsgRXQ;
 	// the storage lists
 	os_mut_init(&mut_msgList);
@@ -120,39 +120,35 @@ __task void InitTask(void){
 	// used spaces
 	lstStr->count = 0;
 	lstStr->first = NULL;
-	lstStr->last  = NULL;
+	lstStr->last = NULL;
 	// lstStrUsed->startAddr = NULL;
-	
+
 	// initialize box for receive queue
 	_init_box(poolRXQ, sizeof(poolRXQ), sizeof(ListNode));
-	
+
 	// This is literally the holy grail part.
 	// give it the pool start (mySRAM_BASE)
 	// the size of the box, which is 1000*sizeof(ListNode) plus some overhead
 	// box size is the sizeof(ListNode).
 	// math taken from the _declare_box() macro for overhead and alignment calcs
-<<<<<<< HEAD
-	_init_box(Storage, (((sizeof(ListNode)+3)/4)*(1000) + 3), sizeof(ListNode));
-=======
-	_init_box(Storage, (((sizeof(ListNode)+3)/4)*(1000) + 3)*4, sizeof(ListNode));
->>>>>>> f4bb8aa860f45016e2cdfca76cddc0734b00fa4e
-	
-	
+	_init_box(Storage, (((sizeof(ListNode)+3) / 4)*(1000) + 3) * 4, sizeof(ListNode));
+
+
 	// List_init(lstRXQ);
 	// List_init(lstStrFree);
-	
+
 	// initialize mailboxes
 	os_mbx_init(&mbx_MsgBuffer, sizeof(mbx_MsgBuffer));
-	
+
 	// initialize tasks
 	idTimerTask = os_tsk_create(TimerTask, 150);		// Timer task is relatively important.
-	idJoyTask 	= os_tsk_create(JoystickTask, 100);
+	idJoyTask = os_tsk_create(JoystickTask, 100);
 	idClockTask = os_tsk_create(ClockTask, 175);		// high prio since we need to timestamp messages
 	idTextRX = os_tsk_create(TextRX, 170);
 	idDispTask = os_tsk_create(DisplayTask, 150);
-	
+
 	GLCD_Clear(Black);
-	
+
 	//sudoku
 	os_tsk_delete_self();
 }
@@ -163,60 +159,49 @@ __task void InitTask(void){
 */
 
 __task void DisplayTask(void){
-<<<<<<< HEAD
-	OS_RESULT error;
-	uint16_t flags;
-	for(;;){
-		flags = os_evt_get();
-		
-			
-	}
-}
-=======
 	uint16_t flag;
-	for(;;){
-		os_evt_wait_or(dispClock|dispUser,0xffff);
+	for (;;){
+		os_evt_wait_or(dispClock | dispUser, 0xffff);
 		flag = os_evt_get();
-		if (flag|dispClock == dispClock){
-			os_mut_wait(&mut_osTimestamp,0xffff);
+		if (flag | dispClock == dispClock){
+			os_mut_wait(&mut_osTimestamp, 0xffff);
 			GLCD_SetBackColor(Black);
 			GLCD_SetTextColor(White);
-			GLCD_DisplayString(0,0,1,timeToString());
+			GLCD_DisplayString(0, 0, 1, timeToString());
 			os_mut_release(&mut_osTimestamp);
 		}
-			
+
 	}
 }
 
 uint8_t* timeToString(void){
-											 // 012345678
+	// 012345678
 	volatile static uint8_t *time = "00:00:00";
 	static uint8_t offset = 0x30;
-	
-	time[0] = osTimestamp.hours/10+offset;
-	time[1] = osTimestamp.hours%10+offset;
-	time[3] = osTimestamp.minutes/10+offset;
-	time[4] = osTimestamp.minutes%10+offset;
-	time[6] = osTimestamp.seconds/10+offset;
-	time[7] = osTimestamp.seconds%10+offset;
-	
+
+	time[0] = osTimestamp.hours / 10 + offset;
+	time[1] = osTimestamp.hours % 10 + offset;
+	time[3] = osTimestamp.minutes / 10 + offset;
+	time[4] = osTimestamp.minutes % 10 + offset;
+	time[6] = osTimestamp.seconds / 10 + offset;
+	time[7] = osTimestamp.seconds % 10 + offset;
+
 	return time;
 }
->>>>>>> f4bb8aa860f45016e2cdfca76cddc0734b00fa4e
 
 // timer task for general-purpose polling and task triggering
 __task void TimerTask(void){
 	static uint32_t counter = 0;
 	static uint32_t secondFreq = 1;				// frequencies in Hz of refresh rate for these events
 	static uint32_t buttonInputFreq = 10;
-	for(;;){
+	for (;;){
 		os_dly_wait(10);	// base freq at 100Hz
 		counter++;
 		// set up if structure to get different timing frequencies
-		if (counter%(100/buttonInputFreq) == 0){	// 100Hz/5Hz = 20, * 10ms = 200ms = 5Hz
+		if (counter % (100 / buttonInputFreq) == 0){	// 100Hz/5Hz = 20, * 10ms = 200ms = 5Hz
 			os_evt_set(timer10Hz, idJoyTask);
 		}
-		if (counter%(100/secondFreq) == 0){// 100*10ms = 1000ms = 1sec
+		if (counter % (100 / secondFreq) == 0){// 100*10ms = 1000ms = 1sec
 			os_evt_set(timer1Hz, idClockTask);
 		}
 		counter %= 100; // makes sure the counter doesn't go over 100 (100*10ms = 1000ms = 1sec)
@@ -225,109 +210,80 @@ __task void TimerTask(void){
 
 __task void ClockTask(void){
 	uint16_t flags;
-	for(;;){
-<<<<<<< HEAD
-		flags = os_evt_get();
-		if (flags & timer1Hz == timer1Hz){
-			os_evt_wait_or(timer1Hz|hourButton|minButton, 0xffff);
-=======
-		os_evt_wait_or(timer1Hz|hourButton|minButton, 0xffff);
+	for (;;){
+		os_evt_wait_or(timer1Hz | hourButton | minButton, 0xffff);
 		flags = os_evt_get();
 		if ((flags & timer1Hz) == timer1Hz){
->>>>>>> f4bb8aa860f45016e2cdfca76cddc0734b00fa4e
-			os_mut_wait(&mut_osTimestamp,0xffff);
-		
+			os_mut_wait(&mut_osTimestamp, 0xffff);
+
 			osTimestamp.seconds++;
-			osTimestamp.minutes += osTimestamp.seconds/60;
+			osTimestamp.minutes += osTimestamp.seconds / 60;
 			osTimestamp.seconds %= 60;
-			osTimestamp.hours 	+= osTimestamp.minutes/60;
+			osTimestamp.hours += osTimestamp.minutes / 60;
 			osTimestamp.minutes %= 60;
-			osTimestamp.hours 	%= 24;
-<<<<<<< HEAD
-			os_evt_clr(timer1Hz, idClockTask);
-		
-			os_mut_release(&mut_osTimestamp);
-			os_evt_set(dispClock,idDispTask);
-		}
-		if (flags & hourButton == hourButton){
-			os_evt_wait_or(timer1Hz|hourButton|minButton, 0xffff);
-=======
-			
+			osTimestamp.hours %= 24;
+
 			os_evt_clr(timer1Hz, idClockTask);
 			flags = os_evt_get();
 			os_mut_release(&mut_osTimestamp);
-			os_evt_set(dispClock,idDispTask);
+			os_evt_set(dispClock, idDispTask);
 		}
 		if ((flags & hourButton) == hourButton){
->>>>>>> f4bb8aa860f45016e2cdfca76cddc0734b00fa4e
-			os_mut_wait(&mut_osTimestamp,0xffff);
-		
+			os_mut_wait(&mut_osTimestamp, 0xffff);
+
 			osTimestamp.hours++;
 			osTimestamp.hours %= 24;
 			os_evt_clr(hourButton, idClockTask);
-<<<<<<< HEAD
-		
-			os_mut_release(&mut_osTimestamp);
-			os_evt_set(dispClock,idDispTask);
-		}
-		if(flags & minButton == minButton){
-			os_evt_wait_or(timer1Hz|hourButton|minButton, 0xffff);
-=======
 			flags = os_evt_get();
 			os_mut_release(&mut_osTimestamp);
-			os_evt_set(dispClock,idDispTask);
+			os_evt_set(dispClock, idDispTask);
 		}
-		if((flags & minButton) == minButton){
->>>>>>> f4bb8aa860f45016e2cdfca76cddc0734b00fa4e
-			os_mut_wait(&mut_osTimestamp,0xffff);
-		
+		if ((flags & minButton) == minButton){
+			os_mut_wait(&mut_osTimestamp, 0xffff);
+
 			osTimestamp.minutes++;
 			osTimestamp.minutes %= 60;
 			os_evt_clr(minButton, idClockTask);
-<<<<<<< HEAD
-		
-=======
 			flags = os_evt_get();
->>>>>>> f4bb8aa860f45016e2cdfca76cddc0734b00fa4e
 			os_mut_release(&mut_osTimestamp);
-			os_evt_set(dispClock,idDispTask);
+			os_evt_set(dispClock, idDispTask);
 		}
 	}
 }
 
 __task void JoystickTask(void){	// TODO:  must get this working after the data handling works.  Should set events for a screen task.
 	static uint32_t newJoy, oldJoy = 0;
-	for(;;){
+	for (;;){
 		//os_sem_wait(&sem_Timer5Hz,0xffff);
 		//while(os_sem_wait(&sem_Timer5Hz,0x0) != OS_R_TMO){}	// clear the semaphore (force binary behavior)
 		// really this shouldn't ever be anything more thana binary semaphore but, just in case.
-		os_evt_wait_and(timer10Hz,0xffff);
-		
+		os_evt_wait_and(timer10Hz, 0xffff);
+
 		newJoy = JOY_GetKeys();
 		//os_mut_wait(&mutCursor,0xffff);
 		if (newJoy != oldJoy){
-			switch(newJoy){
-				case JOY_DOWN:	// cursor left
-					//CursorPos.col = (CursorPos.col+15)%16;	// edited to be the same as the other, for consistency.
-					break;
-				case JOY_UP:	// cursor right
-					//CursorPos.col = (CursorPos.col+1)%16;
-					break;
-				case JOY_LEFT:	// cursor up
-					//CursorPos.row = (CursorPos.row+5)%6;	// modulus by non-powers-of-2 is screwed up in a binary system without floats (F%6 = 3, not 6)
-					break;
-				case JOY_RIGHT:	// cursor down
-					//CursorPos.row = (CursorPos.row+1)%6;
-					break;
-				default:
-					break;
+			switch (newJoy){
+			case JOY_DOWN:	// cursor left
+				//CursorPos.col = (CursorPos.col+15)%16;	// edited to be the same as the other, for consistency.
+				break;
+			case JOY_UP:	// cursor right
+				//CursorPos.col = (CursorPos.col+1)%16;
+				break;
+			case JOY_LEFT:	// cursor up
+				//CursorPos.row = (CursorPos.row+5)%6;	// modulus by non-powers-of-2 is screwed up in a binary system without floats (F%6 = 3, not 6)
+				break;
+			case JOY_RIGHT:	// cursor down
+				//CursorPos.row = (CursorPos.row+1)%6;
+				break;
+			default:
+				break;
 			}
 			// send some event here
 			oldJoy = newJoy;
 		}
 		//os_mut_release(&mutCursor);
 		//os_sem_send(&semDisplayUpdate);
-	
+
 	}
 }
 
@@ -335,18 +291,18 @@ __task void JoystickTask(void){	// TODO:  must get this working after the data h
 __task void TextRX(void){
 	static ListNode	*newmsg;
 	static ListNode *message;
-	for(;;){
+	for (;;){
 		os_mbx_wait(&mbx_MsgBuffer, (void **)&newmsg, 0xffff);
 		os_mut_wait(&mut_osTimestamp, 0xffff);
 		os_mut_wait(&mut_msgList, 0xffff);
-		
+
 		message = _alloc_box(Storage);
 		message->data = newmsg->data;
 		message->data.time = osTimestamp;
 		List_push(lstStr, message);	// put our thing as the most recent message
 		// TODO:  implement a "You've Got Mail" counting semaphore maybe.
 		_free_box(poolRXQ, newmsg);
-		
+
 		os_mut_release(&mut_osTimestamp);
 		os_mut_release(&mut_msgList);
 	}
@@ -362,7 +318,7 @@ __task void TextRX(void){
 
 void SerialInit(void){
 	SER_Init();
-	NVIC->ISER[USART3_IRQn/32] = (uint32_t)1<<(USART3_IRQn%32); // enable USART3 IRQ
+	NVIC->ISER[USART3_IRQn / 32] = (uint32_t)1 << (USART3_IRQn % 32); // enable USART3 IRQ
 	NVIC->IP[USART3_IRQn] = 0xE0; // set priority for USART3 to 0xE0
 	USART3->CR1 |= USART_CR1_RXNEIE; // enable device interrupt for data received and ready to read
 }
@@ -373,12 +329,12 @@ void USART3_IRQHandler(void){
 	static uint8_t countData = 0;	// our place in the buffer
 	static uint8_t sendflag = FALSE;
 	static ListNode *rxnode;
-	
+
 	uint8_t flag = USART3->SR & USART_SR_RXNE; // make a flag for the USART data buffer full & ready to read signal
 
-	if(flag == USART_SR_RXNE){	// if the flag is set.  If not, do nothing.
+	if (flag == USART_SR_RXNE){	// if the flag is set.  If not, do nothing.
 		data = SER_GetChar();
-		if(isr_mbx_check(mbx_MsgBuffer) > 0){
+		if (isr_mbx_check(mbx_MsgBuffer) > 0){
 			if (data >= 0x20 && data <= 0x7E){	// exclude the backspace key, include space.  TODO implement a "backspace" feature!
 				databuff.text[countData] = data; // store serial input to data buffer
 				countData++;
@@ -388,23 +344,25 @@ void USART3_IRQHandler(void){
 					// send to mailbox
 					sendflag = TRUE;
 				}
-			} else if (data == 0x7F){
+			}
+			else if (data == 0x7F){
 				// backspace character
-				countData = (countData + 159)%160;	// move the cursor back one, and clamp at zero
-				
-			} else if (data == 0x0D){
+				countData = (countData + 159) % 160;	// move the cursor back one, and clamp at zero
+
+			}
+			else if (data == 0x0D){
 				// return
 				databuff.cnt = countData;
 				countData = 0;
 				// send to mailbox
 				sendflag = TRUE;
 			}
-			
+
 			if (sendflag == TRUE){
 				rxnode = _alloc_box(poolRXQ);	// TODO: change to use OS stuff
 				rxnode->data = databuff;
 				isr_mbx_send(&mbx_MsgBuffer, rxnode);
 			}
 		}
-	}		
+	}
 }
